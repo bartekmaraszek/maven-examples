90f2a870570caef5d2c2305838c6d9ff44aae5e9
Bart Maraszek <bart.maraszek@imanage.com>
2021-08-18 21:22:22 +0100

    Java and Groovy example

---
+++ src/main/groovy/GroovyHello.groovy
@@ -0,0 +1,8 @@
+import com.bmaraszek.HelloWorld
+
+class GroovyHello {
+    static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld()
+        println helloWorld.hello()
+    }
+}
